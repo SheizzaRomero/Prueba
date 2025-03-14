a5727db282dd9df20cdcbcc3f774f15b65de5f43
SheizzaRomero <sheizza.romero@cepbuna.edu.py>
2025-03-14 12:35:40 -0300

    Update modulo1.py

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,6 +1,7 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
 print ("Hola mundo")
+print ("Hola hola")
 
 
 
